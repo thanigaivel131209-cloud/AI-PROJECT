ab94333949a97e2b90e30b7812ac7308c84216e0
thanigaivel131209-cloud <thanigaivel131209@gmail.com>
2026-06-14 22:19:35 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Renewable Energy Output Predictor",
+    page_icon="🌱"
+)
+
+st.title("🌱 Renewable Energy Output Predictor")
+
+st.write("Welcome to the Renewable Energy Output Predictor Project.")
